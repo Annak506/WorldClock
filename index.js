7aa9a2096a2a8237dfa4b1b114ddbb7d8e8da0e5
Annak506 <annakleinloog@hotmail.com>
2024-01-15 16:00:15 +0300

    updated mistake so current location is not undefined

--- index.js
+++ index.js
@@ -23,31 +23,29 @@ if(losAngelesElement){
     );
 }}
 
-function updateCity(event){
-    
-    let cityTimeZone = event.target.value;
-    if(cityTimeZone == "now"){
-        cityTimezone = moment.tz.guess();
-        console.log(cityTimeZone);
-    }
-
-    let cityName = cityTimeZone.replace('_', ' ').split("/")[1];
-    let cityTime = moment().tz(cityTimeZone);
-    let citiesElement = document.querySelector("#cities");
-    citiesElement.innerHTML =  `
-    <div class="city" >
-        <div>
-            <h2>${cityName} </h2>
-            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-        </div>
-    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.
-        format(
-        "A"
-    )}</small></div>
+function updateCity(event) {
+    setInterval(function(){
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
-    `;
-}; 
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+})};
 
+updateTime();
 setInterval(updateTime, 1000); 
 
 let citiesSelectElement = document.querySelector("#city");
